25e42a36ec36f6bf1792aac8c30091e04d437a65
Fawzy Moawad <fawzy3arafa88@gmail.com>
2023-08-08 14:46:00 -0700

    add the project

---
+++ ASP.NET MVC Entity Framework Assignment Part 4/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_MVC_Entity_Framework_Assignment_Part_4.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
+
